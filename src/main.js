e9759e725851ee60f762335cc2070ffb67be53bf
LiuKairui <liukairui.855@bytedance.com>
2025-06-25 15:49:59 +0800

    done

--- src/main.js
+++ src/main.js
@@ -15,41 +15,8 @@ let inlineViewerHelper = null;
 let gl = null;
 let renderer = null;
 let scene = new Scene();
+let isXrImmersiveMode = false;
 scene.enableStats(false);
-
-let video = document.createElement('video');
-video.loop = true;
-video.src = 'media/video/bbb-sunflower-540p2-1min.webm';
-
-let videoNode = new VideoNode({
-    video: video,
-    displayMode: 'stereoTopBottom',
-});
-
-// When the video is clicked we'll pause it if it's playing.
-videoNode.onSelect(() => {
-    if (!video.paused) {
-        playButton.visible = true;
-        video.pause();
-    } else {
-        playButton.visible = false;
-        video.play();
-    }
-});
-videoNode.selectable = true;
-
-videoNode.translation = [0.025, 0.275, -4.4];
-videoNode.scale = [2.1, 1.1, 1.0];
-scene.addNode(videoNode);
-
-video.addEventListener('loadeddata', () => {
-    let aspect = videoNode.aspectRatio;
-    if (aspect < 2.0) {
-        videoNode.scale = [aspect * 1.1, 1.1, 1.0];
-    } else {
-        videoNode.scale = [2.1, 2.1 / aspect, 1.0];
-    }
-});
 
 let playTexture = new UrlTexture('media/textures/play-button.png');
 
@@ -63,6 +30,100 @@ playButton.visible = false;
 playButton.translation = [0.025, 0.275, -4.2];
 playButton.scale = [5.0, 5.0, 5.0];
 scene.addNode(playButton);
+
+/// ============================================================================
+
+let fileInput = document.createElement('input');
+fileInput.type = 'file';
+fileInput.accept = 'video/*';
+fileInput.style.display = 'none';
+document.body.appendChild(fileInput);
+
+fileInput.addEventListener('change', (ev) => {
+    const file = event.target.files[0];
+    if (file) {
+        instanceVideo(URL.createObjectURL(file), "mono");
+    }
+});
+
+let fileButtonTexture = new UrlTexture('media/textures/Box.png');
+const fileButton = new ButtonNode(fileButtonTexture, () => {
+    fileInput.click();
+});
+fileButton.scale = [5, 5, 5];
+fileButton.translation = [-1.5, -1.2, -4.2];
+scene.addNode(fileButton);
+
+/// ============================================================================
+
+let upDownButtonTexture = new UrlTexture('media/textures/TB.png');
+const upDownButton = new ButtonNode(upDownButtonTexture, () => {
+    instanceVideo(videoElement.src, "stereoTopBottom");
+});
+upDownButton.scale = [5, 5, 5];
+upDownButton.translation = [-0.5, -1.2, -4.2];
+scene.addNode(upDownButton);
+
+let leftRightButtonTexture = new UrlTexture('media/textures/LR.png');
+const leftRightButton = new ButtonNode(leftRightButtonTexture, () => {
+    instanceVideo(videoElement.src, "stereoLeftRight");
+});
+leftRightButton.scale = [5, 5, 5];
+leftRightButton.translation = [0.5, -1.2, -4.2];
+scene.addNode(leftRightButton);
+
+let MonoButtonTexture = new UrlTexture('media/textures/Mono.png');
+const MonoButton = new ButtonNode(MonoButtonTexture, () => {
+    instanceVideo(videoElement.src, "mono");
+});
+MonoButton.scale = [5, 5, 5];
+MonoButton.translation = [1.5, -1.2, -4.2];
+scene.addNode(MonoButton);
+
+/// ============================================================================
+
+let videoElement = null;
+let videoNode = null;
+function instanceVideo(videoUrl, mode) {
+    videoElement?.remove();
+    scene.removeNode(videoNode);
+
+    videoElement = document.createElement('video');
+    videoElement.loop = true;
+    videoElement.src = videoUrl;
+
+    videoNode = new VideoNode({
+        video: videoElement,
+        displayMode: mode, // "stereoLeftRight" || "stereoTopBottom" || "mono",
+    });
+
+    videoNode.onSelect(() => {
+        if (!videoElement.paused) {
+            playButton.visible = true;
+            videoElement.pause();
+        } else {
+            playButton.visible = false;
+            videoElement.play();
+        }
+    });
+    videoNode.selectable = true;
+
+    videoNode.translation = [0.025, 0.275, -4.4];
+    videoNode.scale = [2.1, 1.1, 1.0];
+    scene.addNode(videoNode);
+
+    videoElement.addEventListener('loadeddata', () => {
+        let aspect = videoNode.aspectRatio;
+        if (aspect < 2.0) {
+            videoNode.scale = [aspect * 1.1, 1.1, 1.0];
+        } else {
+            videoNode.scale = [2.1, 2.1 / aspect, 1.0];
+        }
+        if (isXrImmersiveMode) {
+            videoElement.play();
+        }
+    });
+}
 
 function initXR() {
     xrButton = new WebXRButton({
@@ -102,8 +163,8 @@ function initGL() {
 function onRequestSession() {
     let pending;
 
-    pending = video.play().then(() => {
-        video.pause();
+    pending = videoElement.play().then(() => {
+        videoElement.pause();
     });
 
     return navigator.xr.requestSession('immersive-vr', {
@@ -114,7 +175,7 @@ function onRequestSession() {
         onSessionStarted(session);
 
         pending.then(() => {
-            video.play();
+            videoElement.play();
         });
     });
 }
@@ -138,6 +199,7 @@ function onSessionStarted(session) {
     session.requestReferenceSpace(refSpaceType).then((refSpace) => {
         if (session.isImmersive) {
             xrImmersiveRefSpace = refSpace;
+            isXrImmersiveMode = true;
         } else {
             inlineViewerHelper = new InlineViewerHelper(gl.canvas, refSpace);
         }
@@ -147,6 +209,7 @@ function onSessionStarted(session) {
 }
 
 function onEndSession(session) {
+    isXrImmersiveMode = false;
     session.end();
 }
 
@@ -175,4 +238,5 @@ function onXRFrame(t, frame) {
     scene.endFrame();
 }
 
+instanceVideo("media/video/bbb-sunflower-540p2-1min.webm", "stereoTopBottom");
 initXR();
